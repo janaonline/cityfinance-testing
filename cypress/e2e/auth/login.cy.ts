91c5b48197ad35f060dc7fa08b3aca1e7a779532
mdshamoon <mohdshamoon841@gmail.com>
2024-08-13 11:28:38 +0530

    added test for ligin

---
+++ cypress/e2e/auth/login.cy.ts
@@ -0,0 +1,28 @@
+describe("Login page", () => {
+    const id = Cypress.env("id");
+    const password = Cypress.env("password");
+
+    beforeEach(function () {
+        cy.visit("/login");
+    });
+
+    it("Load the login page", () => {
+        cy.get("span").should("contain", "Users");
+    });
+
+    it("Check validations", () => {
+        cy.get("#ulb").click();
+        cy.get(".formTitle").should("contain", "Sign In");
+        cy.get(".login-btn").eq(0).click();
+        cy.get("input[type='email']").should("have.class", "ng-invalid");
+    });
+
+    it("Successful login", () => {
+        cy.get("#ulb").click();
+        cy.get("input[type=email]").type(id);
+        cy.get("input[type=password]").type(password);
+        cy.get(".login-btn").eq(0).click();
+        cy.wait(2000)
+        cy.get("h3").should("contain", "Select Financial Year");
+    });
+});
