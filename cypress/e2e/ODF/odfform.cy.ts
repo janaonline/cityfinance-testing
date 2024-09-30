8946b0aab73bc0b3130547350fc41c0fbdd97c8d
Abhishek60764 <abhishek.a@janaagraha.org>
2024-09-30 15:52:18 +0530

    Revised GFC and ODF form

--- cypress/e2e/ODF/odfform.cy.ts
+++ cypress/e2e/ODF/odfform.cy.ts
@@ -250,7 +250,8 @@ describe("test1", () => {
     });
   });
 
-  it("Form is successfully submit", () => {
+
+  it("Form is successfully submitted", () => {
     cy.visit("https://staging.cityfinance.in/login");
     cy.get("#ulb i").click();
     cy.get('input[formcontrolname="email"]').type(Cypress.env("odfid"));
@@ -266,11 +267,13 @@ describe("test1", () => {
     cy.get("mat-select[id='1']").click();
     cy.get("mat-option").contains("span", "ODF++").click();
     cy.xpath("//button[normalize-space()='Upload PDF']").click();
-    cy.xpath("//button[normalize-space()='Upload PDF']").attachFile('ODF.pdf', { force: true });
-
+    cy.get("input[accept$='application/pdf, 5120, 1']").attachFile("ODF.pdf", {
+      force: true,
+    });
     cy.get('input[type="date"]').then(($input: JQuery<HTMLInputElement>) => {
       // Set the value of the input field
       $input[0].value = '2023-09-10';  
+      
   
       // Dispatch the 'input' event
       $input[0].dispatchEvent(new Event('input', { bubbles: true }));
@@ -278,25 +281,277 @@ describe("test1", () => {
       // Dispatch the 'change' event
       $input[0].dispatchEvent(new Event('change', { bubbles: true }));
   });
+    cy.wait(10000);
+    cy.xpath("//button[normalize-space()='Save as Draft']").click();
+    cy.get(".swal-icon.swal-icon--success").should("be.visible");
+    cy.get(".swal-title").should("be.visible").and("have.text", "Saved");
+    cy.get(".swal-text")
+      .should("be.visible")
+      .and("have.text", "Data saved as draft successfully");
+      cy.get(".swal-button.swal-button--confirm").click();
+      cy.xpath("//button[normalize-space()='Submit']").click();
+      cy.get(".swal-icon.swal-icon--warning").should("be.visible");
+      cy.get(".swal-title").should('have.text',"Confirmation !");
+      cy.get(".swal-button.swal-button--Submit").click();
+      cy.wait(1000);
+
+  })
 
 
-    cy.get('input[type="date"]').click();
+  it('Check Review grant application UI',()=>{
+    cy.visit("https://staging.cityfinance.in/login");
+    cy.get("#state").click();
+    cy.get('input[formcontrolname="email"]').type(Cypress.env('odfstate_id'));
+    cy.get('input[formcontrolname="password"]').type(Cypress.env('odfstate_password'));
+    cy.get("button[type='submit']").click();
+    cy.contains('a', '15th FC Grants').click();
+    cy.contains('button', '2024-25').click();
+    cy.contains('a', 'Review Grant Application').click(); 
+    cy.get('#form').select('Open Defecation Free (ODF)');
+    cy.get('.header2').should('contain.text',' Review Grant Application ');
+    cy.contains('button', 'Approve all').should("be.visible");
+    cy.contains('button', 'Return all').should("be.visible");
+    cy.contains('button', 'Download').should("be.visible");
+    cy.contains('button', 'Reset').should("be.visible");
+    cy.contains('a','Take Action').should("be.visible");
+    // cy.get('.mat-checkbox').should("be.visible");
+    cy.get('thead tr:nth-child(1)').should('have.css', 'background-color', 'rgb(4, 116, 116)')
+    .and('have.css', 'color', 'rgb(255, 255, 255)');
+    cy.get('thead tr:nth-child(1)')
+        .should('be.visible') 
+        .within(() => {
+         
+         
+          
+          cy.get('th.ng-star-inserted').eq(0).should('contain.text', 'S No.');
+          cy.get('th.ng-star-inserted').eq(1).should('contain.text', 'ULB Name');
+          cy.get('th.ng-star-inserted').eq(2).should('contain.text', 'Census/SB Code');
+          cy.get('th.ng-star-inserted').eq(3).should('contain.text', 'ULB Type');
+          cy.get('th.ng-star-inserted').eq(4).should('contain.text', 'Population Type');
+          cy.get('th.ng-star-inserted').eq(5).should('contain.text', 'UA');
+          cy.get('th.ng-star-inserted').eq(6).should('contain.text', 'Form Status');
+          cy.get('th.ng-star-inserted').eq(7).should('contain.text', 'Filled Status');
+          cy.get('th.ng-star-inserted').eq(8).should('contain.text', 'Action');
+        });
+  })
+
+
+  it('ODF Preview UI',()=>{
+    cy.visit("https://staging.cityfinance.in/login");
+    cy.get("#state").click();
+    cy.get('input[formcontrolname="email"]').type(Cypress.env('odfstate_id'));
+    cy.get('input[formcontrolname="password"]').type(Cypress.env('odfstate_password'));
+    cy.get("button[type='submit']").click();
+    cy.contains('a', '15th FC Grants').click();
+    cy.contains('button', '2024-25').click();
+    cy.contains('a', 'Review Grant Application').click(); 
+    cy.get('#form').select('Open Defecation Free (ODF)');
+    cy.get("input[placeholder='Ulb Code']").type(Cypress.env('odfid'));
+    cy.get("input[placeholder='Ulb Code']").next().click();
+    cy.contains('a','Take Action').click();
+    cy.contains('button','Preview').click();
+    cy.xpath("//span[normalize-space()='Submissions for 15th FC grant for FY 2024-25']").
+      should('be.visible').and('have.text',' Submissions for 15th FC grant for FY 2024-25 ');
+      cy.get('#donwloadButton').should('be.visible');
+      cy.get(".form-h.text-center").
+      should('be.visible').and('have.text',' Open Defecation Free (ODF) ');
+      cy.get('.form-status').should("be.visible");
+      cy.get('.d-ans').each(($el) => {
+     
+        cy.wrap($el).should('be.visible');
+      });
+      
+  
+  })
     
+  
+  })
+//   it('Verify the error message displayed when submitting the Review Grant Application State ODF form without a status',()=>{
+//     cy.visit("https://staging.cityfinance.in/login");
+//     cy.get("#state").click();
+//     cy.get('input[formcontrolname="email"]').type(Cypress.env('odfstate_id'));
+//     cy.get('input[formcontrolname="password"]').type(Cypress.env('odfstate_password'));
+//     cy.get("button[type='submit']").click();
+//     cy.contains('a', '15th FC Grants').click();
+//     cy.wait(3000);
+//     cy.contains('button', '2024-25').click();
+//     cy.contains('a', 'Review Grant Application').click(); 
+//     cy.get('#form').select('Open Defecation Free (ODF)');
+//     cy.get("input[placeholder='Ulb Code']").type(Cypress.env('code'));
+//     cy.get("input[placeholder='Ulb Code']").next().click();
+//     cy.contains('a','Take Action').click();
+//     cy.contains('button','Submit').click();
+//     cy.get(".swal-icon.swal-icon--error").should("be.visible");
+//     cy.get('.swal-title').should('contain.text', 'Error');
+//     cy.get('.swal-text').should('contain.text', 'Status is mandatory');
+//     cy.get('.swal-button-container').click();
+//     cy.get('.text-danger').should('contain.text', ' This is the required field. ');
+  
+//   })
+//   it('Verify the error message displayed when returning the ODF form by state without providing a reason.',()=>{
+//     cy.visit("https://staging.cityfinance.in/login");
+//     cy.get("#state").click();
+//     cy.get('input[formcontrolname="email"]').type(Cypress.env('odfstate_id'));
+//     cy.get('input[formcontrolname="password"]').type(Cypress.env('odfstate_password'));
+//     cy.get("button[type='submit']").click();
+//     cy.contains('a', '15th FC Grants').click();
+//     cy.wait(3000);
+//     cy.contains('button', '2024-25').click();
+//     cy.contains('a', 'Review Grant Application').click(); 
+//     cy.get('#form').select('Open Defecation Free (ODF)');
+//     cy.get("input[placeholder='Ulb Code']").type(Cypress.env('code'));
+//     cy.get("input[placeholder='Ulb Code']").next().click();
+//     cy.contains('a','Take Action').click();
+//     cy.contains('label','Return').click();
+//     cy.contains('button','Submit').click();
+//     cy.get(".swal-icon.swal-icon--error").should("be.visible");
+//     cy.get('.swal-title').should('contain.text', 'Error');
+//     cy.get('.swal-text').should('contain.text', 'Reject reason is mandatory');
+//     cy.get('.swal-button-container').click();
+//     cy.get('.text-danger').should('contain.text', ' This is the required field. ');
+  
+//   })
+//   it('Verify that the Odf form is successfully returned by state.',()=>{
+//     cy.visit("https://staging.cityfinance.in/login");
+//     cy.get("#state").click();
+//     cy.get('input[formcontrolname="email"]').type(Cypress.env('odfstate_id'));
+//     cy.get('input[formcontrolname="password"]').type(Cypress.env('odfstate_password'));
+//     cy.get("button[type='submit']").click();
+//     cy.contains('a', '15th FC Grants').click();
+//     cy.wait(3000);
+//     cy.contains('button', '2024-25').click();
+//     cy.contains('a', 'Review Grant Application').click(); 
+//     cy.get('#form').select('Open Defecation Free (ODF)');
+//     cy.get("input[placeholder='Ulb Code']").type(Cypress.env('code'));
+//     cy.get("input[placeholder='Ulb Code']").next().click();
+//     cy.contains('a','Take Action').click();
+//     cy.get("#reason").type("The uploaded document is incorrect.");
+//     cy.get("input[accept$='.pdf']").next().click();
+//     cy.get("input[accept$='.pdf']")
+//     .attachFile('ODF.pdf', { force: true });
+//     cy.contains('label','Return').click();
+//     cy.contains('button','Submit').click();
+//     cy.get(".swal-button.swal-button--Submit").click();
+     
+//   })
+  
+//   it('ODF form Submit',()=>{
+//     cy.visit("https://staging.cityfinance.in/login");
+//     cy.get("#ulb i").click();
+//     cy.get('input[formcontrolname="email"]').type(Cypress.env('code'));
+//     cy.get('input[formcontrolname="password"]').type(Cypress.env('password'));
+//     cy.get("button[type='submit']").click();
+//     cy.contains('a', '15th FC Grants').click();
+  
+//     cy.contains('button', '2024-25').click();
    
-      cy.xpath("//button[normalize-space()='Save as Draft']").click();
-      cy.get(".swal-icon.swal-icon--success").should("be.visible");
-      cy.get(".swal-title").should("be.visible").and("have.text", "Saved");
-      cy.get(".swal-text")
-        .should("be.visible")
-        .and("have.text", "Data saved as draft successfully");
-        cy.get(".swal-button.swal-button--confirm").click();
-        cy.xpath("//button[@class='btn btn-save text-uppercase ng-star-inserted'][normalize-space()='Submit']").click();
-        cy.get(".swal-icon.swal-icon--warning").should("be.visible");
-        cy.get(".swal-title").should('have.text',"Confirmation !");
-        cy.get(".swal-button.swal-button--Submit").click();
-        cy.wait(1000);
+//     cy.get('a').contains('span', 'Open Defecation Free (ODF)').click();
+//     cy.wait(1000);
+//     cy.get("mat-select[id='1']").click();
+//     cy.get('mat-option').contains('span', 'ODF').click();
+//     cy.xpath("//button[normalize-space()='Upload PDF']").click();
+//     cy.get("input[accept$='application/pdf, 5120, 1']")
+//   .attachFile('ODF.pdf', { force: true });
+  
+//   cy.get('input[type="date"]').then(($input: JQuery<HTMLInputElement>) => {
+//     // Set the value of the input field
+//     $input[0].value = '2023-09-10';  
 
-    });
+//     // Dispatch the 'input' event
+//     $input[0].dispatchEvent(new Event('input', { bubbles: true }));
+
+//     // Dispatch the 'change' event
+//     $input[0].dispatchEvent(new Event('change', { bubbles: true }));
+// });
+   
+//     cy.wait(2000);
+   
+//     cy.xpath("//button[normalize-space()='Submit']").click();
+//     cy.get(".swal-icon.swal-icon--warning").should("be.visible");
+//     cy.get(".swal-title").should('have.text',"Confirmation !");
+//     cy.get(".swal-button.swal-button--Submit").click();
+//     cy.wait(1000);
+//   })
+//     it('check box functionality',()=>{
+//       cy.visit("https://staging.cityfinance.in/login");
+//         cy.get("#state").click();
+//         cy.get('input[formcontrolname="email"]').type(Cypress.env('state_id'));
+//         cy.get('input[formcontrolname="password"]').type(Cypress.env('state_password'));
+//         cy.get("button[type='submit']").click();
+//         cy.contains('a', '15th FC Grants').click();
+//         cy.contains('button', '2024-25').click();
+//         cy.contains('a', 'Review Grant Application').click(); 
+//         cy.get('#form').select('Open Defecation Free (ODF)');
+//         cy.get("input[placeholder='Ulb Code']").type(Cypress.env('code'));
+//         cy.get("input[placeholder='Ulb Code']").next().click();
+//         cy.get('.mat-checkbox').click();
+//         cy.contains('button', 'Return all').click();
+//         cy.get('.card').should("be.visible");
+//         cy.get('.a-h h4').should('contain.text','Action Form');
+//         cy.get("#reason").type("The uploaded document is incorrect.");
+//         cy.get("label[for='cin3'] a").click();
+//         cy.get("#cin3").attachFile('ODF.pdf', { force: true });
+//         cy.get("button[type='submit']").click();
+//         cy.get('.swal-title').should('contain.text','Confirmation');
+//         cy.get("div[role='dialog']").should("be.visible");
+//         cy.get('.swal-button.swal-button--Submit').click();
+//     })
+//     it('ODF form Submit',()=>{
+//       cy.visit("https://staging.cityfinance.in/login");
+//       cy.get("#ulb i").click();
+//       cy.get('input[formcontrolname="email"]').type(Cypress.env('code'));
+//       cy.get('input[formcontrolname="password"]').type(Cypress.env('password'));
+//       cy.get("button[type='submit']").click();
+//       cy.contains('a', '15th FC Grants').click();
+//       cy.wait(3000);
+//       cy.contains('button', '2024-25').click();
+     
+//       cy.get('a').contains('span', 'Open Defecation Free (ODF)').click();
+//       cy.wait(1000);
+//       cy.get("mat-select[id='1']").click();
+//       cy.get('mat-option').contains('span', 'ODF').click();
+//       cy.xpath("//button[normalize-space()='Upload PDF']").click();
+//       cy.get("input[accept$='application/pdf, 5120, 1']")
+//     .attachFile('ODF.pdf', { force: true });
+    
+//     cy.get('input[type="date"]').then(($input: JQuery<HTMLInputElement>) => {
+//       // Set the value of the input field
+//       $input[0].value = '2023-09-10';  
+  
+//       // Dispatch the 'input' event
+//       $input[0].dispatchEvent(new Event('input', { bubbles: true }));
+  
+//       // Dispatch the 'change' event
+//       $input[0].dispatchEvent(new Event('change', { bubbles: true }));
+//   });
+     
+//       cy.wait(2000);
+     
+//       cy.xpath("//button[normalize-space()='Submit']").click();
+//       cy.get(".swal-icon.swal-icon--warning").should("be.visible");
+//       cy.get(".swal-title").should('have.text',"Confirmation !");
+//       cy.get(".swal-button.swal-button--Submit").click();
+//       cy.wait(1000);
+//     })
+//     it('Take Action Functionality Approving Form',()=>{
+//       cy.visit("https://staging.cityfinance.in/login");
+//       cy.get("#state").click();
+//       cy.get('input[formcontrolname="email"]').type(Cypress.env('state_id'));
+//       cy.get('input[formcontrolname="password"]').type(Cypress.env('state_password'));
+//       cy.get("button[type='submit']").click();
+//       cy.contains('a', '15th FC Grants').click();
+//       cy.wait(3000);
+//       cy.contains('button', '2024-25').click();
+//       cy.contains('a', 'Review Grant Application').click(); 
+//       cy.get('#form').select('Open Defecation Free (ODF)');
+//       cy.get("input[placeholder='Ulb Code']").type(Cypress.env('code'));
+//       cy.get("input[placeholder='Ulb Code']").next().click();
+//       cy.contains('a','Take Action').click();
+//       cy.contains('label','Approve').click();
+//       cy.contains('button','Submit').click();
+//       cy.get(".swal-button.swal-button--Submit").click();
+       
+//     })
 
     
-});
+
