4ee4fbf6cc8878f125c3c89bca145bd2436d2b42
Abhishek60764 <abhishek.a@janaagraha.org>
2026-03-25 12:45:09 +0530

    New changes

--- cypress/e2e/GFCFORM/gfc.cy.ts
+++ cypress/e2e/GFCFORM/gfc.cy.ts
@@ -264,7 +264,6 @@ describe("GFC form", () => {
     cy.contains("button", "2024-25").click()
     cy.wait(3000);
 
-    cy.wait(1000);
     cy.get("a").contains("span", "Garbage Free City (GFC)").click();
     cy.wait(1000);
     cy.get("mat-select[id='1']").click();
